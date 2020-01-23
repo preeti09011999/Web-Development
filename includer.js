a486b171bcec9fc384f0d5a642351124c597de18
Preeti <preeti09011999@gmail.com>
2020-01-23 15:29:57 +0530

    filtering odd values

--- includer.js
+++ includer.js
@@ -1,14 +1,21 @@
+// Q4 Write includer function that accepts isOdd  function and filter even values from actual array without mutating actual array
+// arr[5,8,14,17,23,6,8]
+// narr[5,17,23]
 const arr = [5,8,14,17,23,6,8];
 const varr = [];
-function includer(n,isOdd){
-        return n;
+function isOdd(n){
+        return n%2 == 1;
     }
 
 
- function isOdd(n){
-     if(n%2 == 0){
-         return True;
+ function includer(arr, isOdd){
+     for(let i = 0;i<arr.length;i++){
+         if(isOdd(arr[i])){
+             varr.push(arr[i])
+         }
      }
+     return varr;
  }
-var srr = arr.map(includer);
-console.log(srr);
+const sarr = includer(arr, isOdd);
+console.log(sarr);
+console.log(arr);
--- secretmessage.js
+++ secretmessage.js
@@ -1,3 +1,9 @@
+// Que Build string using map function  first character of each string of array
+
+// var animals = ['Hen','elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+
+
 var animals = ['Hen','elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 function secretMessage(str){
 
