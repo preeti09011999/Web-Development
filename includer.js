a1075d29a9ed83d80c12269267a22d0af379ef5d
Preeti <preeti09011999@gmail.com>
2020-01-23 15:14:31 +0530

    secret message problem

---
+++ includer.js
@@ -0,0 +1,14 @@
+const arr = [5,8,14,17,23,6,8];
+const varr = [];
+function includer(n,isOdd){
+        return n;
+    }
+
+
+ function isOdd(n){
+     if(n%2 == 0){
+         return True;
+     }
+ }
+var srr = arr.map(includer);
+console.log(srr);
---
+++ secretmessage.js
@@ -0,0 +1,9 @@
+var animals = ['Hen','elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+function secretMessage(str){
+
+    var ast = str.charAt(0);
+    return ast;
+}
+
+const arr = animals.map(secretMessage);
+console.log(arr.join(""));
