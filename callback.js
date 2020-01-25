f534d4f09aa79db0141d3cec260ffb5fad499704
Preeti <preeti09011999@gmail.com>
2020-01-25 23:32:39 +0530

    Callback function

---
+++ callback.js
@@ -0,0 +1,12 @@
+//synchronous code
+console.log("before code");
+//async code 
+setTimeout(function fn1() {
+    console.log("I will run after 4 sec");
+}, 4000);
+setTimeout(function fn2() {
+    console.log("I will run after 2 sec");    
+}, 2000);
+
+//synchronous code
+console.log("After code");
