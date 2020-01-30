17ee5be0650133aef194599d14f3c46892e1a644
Preeti <preeti09011999@gmail.com>
2020-01-30 14:52:51 +0530

    Weather information from server

--- axios.js
+++ axios.js
@@ -6,8 +6,12 @@
 const axios = require('axios');
 async function helper(){
     // makes a http get request to a server
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const response = await axios.get('https://www.metaweather.com/api/location/search/?query=london');
     // your requested information will be inside data key
     console.log(response.data);
+    // woeid
+    const woeid = response.data[0].woeid
+    const anotherResponse = await axios.get('https://www.metaweather.com/api/location/'+ woeid + "/");
+    console.log(anotherResponse)
 }
 helper();
