2275d9670ad4c3e2b26bf326d84461a072c700f1
Preeti <preeti09011999@gmail.com>
2020-02-01 18:39:44 +0530

    Help menu for the weather forecast application

---
+++ cmds/help.js
@@ -0,0 +1,9 @@
+// to export anything
+module.exports = function(){
+    console.log(`weather [command]
+    <Options>
+    today.....show weather for today
+    forecast.....show 5-day weather forecast
+    version.....show package version
+    help.....show help menu for a command  `);
+};
